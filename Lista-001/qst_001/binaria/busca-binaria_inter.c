010ad95083361d0751cc4483a14e7ecc6dd38e75
JP-Pardinho <jppardinhor@gmail.com>
2025-05-05 15:56:54 -0300

    Aula 1

---
+++ Lista-001/qst_001/binaria/busca-binaria_inter.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int *vet = NULL;
+    int tam_vet;
+    int i;
+
+    printf("Digite o tamanho do vetor: ");
+    scanf("%d", &tam_vet);
+
+    vet = (int *)malloc(tam_vet *sizeof(int));
+
+    for(i=0; i<tam_vet; i++){
+        printf("Digite o %dº numero: ", i+1);
+        scanf("%d", &vet[i]);
+    }
+
+    
+    
+    free(vet);
+    return 0;
+}       
